eb350a4bf456604c78d261a558693db53b97525e
nikronsakha <nikitborisov15@gmail.com.com>
2024-02-23 18:05:08 +0600

    изменение header

--- resources/views/main/partials/header.blade.php
+++ resources/views/main/partials/header.blade.php
@@ -2,8 +2,8 @@
 <nav class="font-sans flex flex-col text-center content-center sm:flex-row sm:text-left sm:justify-between py-2 px-6 bg-white shadow sm:items-baseline w-full">
     <div class="mb-2 sm:mb-0 inner">
 
-        <a href="{{ route('home') }}" class="text-2xl no-underline text-grey-darkest hover:text-blue-dark font-sans font-bold">Laravel с нуля</a><br>
-        <span class="text-xs text-grey-dark">Уроки от CutCode</span>
+        <a href="{{ route('home') }}" class="text-2xl no-underline text-grey-darkest hover:text-blue-dark font-sans font-bold">Блог</a><br>
+        <span class="text-xs text-grey-dark">Блог</span>
 
     </div>
     <div class="sm:mb-0 self-center">
